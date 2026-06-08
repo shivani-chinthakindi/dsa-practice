ece2914ff6c2ee01607fc0176a114aa5ee3740a7
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-08 16:33:59 +0530

    Update sum of the array elements program

--- 01-Arrays/Basics/SumOfElements.java
+++ 01-Arrays/Basics/SumOfElements.java
@@ -1,1 +1,10 @@
-
+class SumOfElements{
+    public static void main(String[] args){
+        int arr[] = {10, 20, 30, 40, 50};
+        int sum = 0;
+        for(int num : arr){
+            sum += num;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
