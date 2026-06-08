725dddf5a5b28ac14a464827f522235f9c56cf59
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-08 17:14:49 +0530

    Update positive and negative numbers count program

--- 01-Arrays/Basics/CountPositiveNegative.java
+++ 01-Arrays/Basics/CountPositiveNegative.java
@@ -1,1 +1,14 @@
-
+class CountPositiveNegative{
+    public static void main(String[] args) {
+        int[] arr = {-1, 2, 3};
+        int posCount = 0, negCount = 0;
+        for(int num : arr){
+            if(num > 0)
+                posCount++;
+            else
+                negCount++;
+        }
+        System.out.println("Total Positive numbers: " + posCount);
+        System.out.println("Total Negative numbers: " + negCount);
+    }
+}
