455e01631bafc3d2b98c3ecaed70aeaed586a67e
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-08 17:10:47 +0530

    Add even and numbers count

--- 01-Arrays/Basics/CountEvenOdd.java
+++ 01-Arrays/Basics/CountEvenOdd.java
@@ -1,1 +1,14 @@
-
+class CountEvenOdd{
+    public static void main(String[] args) {
+        int evenCount = 0, oddCount = 0;
+        int[] arr = {10, 20, 30, 40, 50};
+        for(int num: arr){
+            if(num % 2 == 0)
+                evenCount++;
+            else
+                oddCount++;
+        }
+        System.out.println("Total Even Numbers: " + evenCount);
+        System.out.println("Total Odd Numbers: " + oddCount);
+    }
+}
