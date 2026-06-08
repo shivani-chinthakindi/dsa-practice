9d3f81f5ecec0eb3843c7db3a199d0ba861e8c29
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-08 17:53:37 +0530

    Add frequency of element program

--- 01-Arrays/Basics/FrequencyOfElement.java
+++ 01-Arrays/Basics/FrequencyOfElement.java
@@ -1,1 +1,15 @@
+import java.util.HashMap;
 
+public class FrequencyOfElement {
+    public static void main(String[] args) {
+        // Example array of numbers
+        int[] arr = {1, 2, 2, 3, 1, 4, 2, 5};
+
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : arr) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        System.out.println("Frequency Map: " + freqMap);
+    }
+}
