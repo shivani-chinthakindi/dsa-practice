4242e412f3c10871d4c798bf72af373924362764
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-08 17:38:58 +0530

    Update second largest element

--- 01-Arrays/Basics/SecondLargest.java
+++ 01-Arrays/Basics/SecondLargest.java
@@ -1,1 +1,23 @@
+class SecondLargest{
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1}; // Example array
 
+int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE; 
+
+for (int num : arr) { 
+    if (num > first) { 
+        second = first; 
+        first = num; 
+    } else if (num > second && num != first) { 
+        second = num; 
+    } 
+}
+
+// Printing the result directly without methods
+if (second == Integer.MIN_VALUE) {
+    System.out.println("There is no second largest distinct element.");
+} else {
+    System.out.println("The second largest element is: " + second);
+}
+    }
+}
