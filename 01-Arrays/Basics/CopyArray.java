c1808d136d799377706bb2d90396e59b28753d90
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-08 17:32:32 +0530

    Complete copying array

--- 01-Arrays/Basics/CopyArray.java
+++ 01-Arrays/Basics/CopyArray.java
@@ -1,1 +1,13 @@
-
+class CopyArray{
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6};
+        int[] copiedArr = new int[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            copiedArr[i] = arr[i]; 
+        }
+        System.out.print("Copied Array: ");
+        for (int num : copiedArr) {
+            System.out.print(num + " ");
+        }
+    }
+}
