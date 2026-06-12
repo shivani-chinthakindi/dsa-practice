eaf846611d08d2022fbafd66866e440eec6da770
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-12 17:58:45 +0530

    Complete insert element

---
+++ 01-Arrays/Manipulation/InserElement.java
@@ -0,0 +1,26 @@
+public class InserElement {
+    static void insertElement(int arr[], int n, int x, int pos){
+        for (int i = n-1; i >= pos; i--)
+            arr[i+1] = arr[i];
+        arr[pos] = x;
+    }
+    public static void main (String[] args){
+        int arr[] = new int[15];
+        arr[0] = 2;
+        arr[1] = 4;
+        arr[2] = 1;
+        arr[3] = 8;
+        arr[4] =5;
+        int n = 5;
+        int x = 10, pos = 2;
+        System.out.println("Before Insertion: ");
+        for(int i = 0; i < n; i++) 
+            System.out.print(arr[i] + " ");
+        insertElement(arr, n, x, pos);
+        n+=1;
+        System.out.println();
+        System.out.println("After Insertion: ");
+        for(int i = 0; i < n; i++) 
+            System.out.print(arr[i] + " ");
+    }
+}
