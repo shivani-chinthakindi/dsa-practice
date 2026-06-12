a4dfeab974933cefd65347d60a5d4ed2557be50c
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-12 16:45:27 +0530

    Add search element

---
+++ 01-Arrays/Manipulation/SearchElement.java
@@ -0,0 +1,19 @@
+class SearchElement {
+    static int findElement (int arr[], int n, int key) {
+        for (int i = 0; i < n; i++){
+            if (arr[i] == key)
+                return i;
+            }
+        return -1;
+        }  
+        public static void main(String[] args) {
+            int arr[] = {12, 34, 10, 6, 40};
+            int n = arr.length;
+            int key = 40;
+            int pos = findElement(arr, n, key);
+            if(pos == -1)
+                System.out.println("Element not found");
+            else 
+                System.out.println("Element found at index " + pos);
+    }
+}
