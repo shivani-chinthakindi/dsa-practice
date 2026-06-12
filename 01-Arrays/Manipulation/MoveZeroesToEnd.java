7f5fda2a3350dccf0bff1742f526d0068c44d037
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-12 19:14:57 +0530

    Add move zeroes to end

---
+++ 01-Arrays/Manipulation/MoveZeroesToEnd.java
@@ -0,0 +1,22 @@
+public class MoveZeroesToEnd {
+    static void pushZeroesToEnd(int[] arr){
+        int n = arr.length;
+        int[] temp = new int[n];
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] != 0)
+                temp[j++] = arr[i];
+        }
+        while(j < n)
+            temp[j++] = 0;
+        for(int i = 0; i < n; i++)
+            arr[i] = temp[i];
+    }
+    public static void main(String[] args) {
+        int[] arr ={1, 2, 0, 4, 3, 0, 5, 0};
+        pushZeroesToEnd(arr);
+        for(int num: arr){
+            System.out.print(num + " ");
+        }
+    }
+}
