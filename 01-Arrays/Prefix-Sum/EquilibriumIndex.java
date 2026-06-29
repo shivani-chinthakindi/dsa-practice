df0b9997d14e6e1f728057c7500282958efbae18
Shivani Patel <schinthakindishivani@gmail.com>
2026-06-29 23:03:46 +0530

    Completed equilibrium index

---
+++ 01-Arrays/Prefix-Sum/EquilibriumIndex.java
@@ -0,0 +1,19 @@
+public class EquilibriumIndex {
+    static int findEq(int[] arr){
+        for(int i = 0; i < arr.length; ++i){
+            int leftSum = 0;
+            for(int j = 0; j < i; j++)
+                leftSum+=arr[j];
+            int rightSum = 0;
+            for(int j = i+1; j < arr.length; j++)
+                rightSum+=arr[j];
+            if(leftSum == rightSum)
+                return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {-7,1,5,2,-4,3,0};
+        System.out.println(findEq(arr));
+    }
+}
