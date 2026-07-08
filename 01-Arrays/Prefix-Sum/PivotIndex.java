efafbcff6d42b9feeedee99db9ed7ec6d1ed06c1
Shivani Patel <schinthakindishivani@gmail.com>
2026-07-08 05:55:34 +0530

    Add pivot index

--- 01-Arrays/Prefix-Sum/PivotIndex.java
+++ 01-Arrays/Prefix-Sum/PivotIndex.java
@@ -1,3 +1,21 @@
 public class PivotIndex {
-    
+    public static int findPivot(int[] nums){
+        int totalSum = 0;
+        int leftSum = 0;
+        for(int num: nums){
+            totalSum += num;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(leftSum == totalSum - leftSum - nums[i]){
+                return i;
+            }
+            leftSum = nums[i]
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,7,3,6,5,6};
+        int res = findPivot(nums);
+        System.out.println("The pivot index is: " + res);
+    }
 }
