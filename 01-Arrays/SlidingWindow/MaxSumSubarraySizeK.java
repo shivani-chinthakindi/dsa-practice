47d6e1b6685919043d46f4839c1c191d81692853
Shivani Patel <schinthakindishivani@gmail.com>
2026-07-28 23:13:04 +0530

    Add max sum subarray size k

---
+++ 01-Arrays/SlidingWindow/MaxSumSubarraySizeK.java
@@ -0,0 +1,22 @@
+public class MaxSumSubarraySizeK {
+    static int maxSubarraySum(int[] arr, int k){
+        int n = arr.length;
+        if(n<k)
+            return -1;
+        int windowSum = 0;
+        for(int i = 0; i < k; i++){
+            windowSum += arr[i];
+        }
+        int maxSum = windowSum;
+        for(int i = k; i < n; i++){
+            windowSum += arr[i] - arr[i-k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,1,5,1,3,2};
+        int k = 3;
+        System.out.println(maxSubarraySum(arr, k));
+    }
+}
